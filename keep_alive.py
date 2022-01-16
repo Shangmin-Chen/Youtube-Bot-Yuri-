eab3b9d98578ba4641f30aac7c15fb7a0086e253
Youngshangsaucy <75769334+Shangmin-Chen@users.noreply.github.com>
2022-01-16 02:54:19 +0000

    front end update

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from threading import Thread
 
 app = Flask(__name__)
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+  if request.method == "POST":
+    link = request.form["link"]
+    title = request.form["title"]
+    video = request.form.get("video")
+    audio = request.form.get("audio")
 
 def run():
   app.run(host='0.0.0.0', port=8080)
